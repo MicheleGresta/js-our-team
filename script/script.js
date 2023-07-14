35f750296df1638f13349f4655086ffdef25d206
MicheleGresta <malachia_1997@hotmail.it>
2023-07-14 16:45:05 +0200

    code adj

--- script/script.js
+++ script/script.js
@@ -1,25 +1,4 @@
 
-const nome1 = document.querySelector("[name='nome1']");
-const nome2 = document.querySelector("[name='nome2']");
-const nome3 = document.querySelector("[name='nome3']");
-const nome4 = document.querySelector("[name='nome4']");
-const nome5 = document.querySelector("[name='nome5']");
-const nome6 = document.querySelector("[name='nome6']");
-
-const role1 = document.querySelector("[name='role1']");
-const role2 = document.querySelector("[name='role2']");
-const role3 = document.querySelector("[name='role3']");
-const role4 = document.querySelector("[name='role4']");
-const role5 = document.querySelector("[name='role5']");
-const role6 = document.querySelector("[name='role6']");
-
-const primaFoto = document.querySelector("[name='foto1']");
-const secondaFoto = document.querySelector("[name='foto2']");
-const terzaFoto = document.querySelector("[name='foto3']");
-const quartaFoto = document.querySelector("[name='foto4']");
-const quintaFoto = document.querySelector("[name='foto5']");
-const sensaFoto = document.querySelector("[name='foto6']");
-
 const teamContainer = document.querySelector("[name='team-container']");
 const cardBody = document.querySelector("[class='card-title']");
 
@@ -56,7 +35,6 @@ const teamMember = [{
 
 for (const key in teamMember) {
     console.log(teamMember[key]);
-    const singleMember = teamMember[key];
 }
 
 memberCard()
